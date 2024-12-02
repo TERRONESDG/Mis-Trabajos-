f2a2b7bf4efa6fdbd75f213291939a06a852e3c3
Diego Andres Terrones Gonzalez <diegoterrones221@gmail.com>
2024-12-02 16:44:30 -0600

    Update and rename act 1,2.py to Programa2.md

---
+++ codigo/bloque 1 y 2/Programa2.md
@@ -0,0 +1,6 @@
+# Programa2. programar que solicita el nombre y lo imprime con un mensaje personalizado]
+
+```python
+nombre = input ("Ingresa tu nombre:")
+print ("Hola" + nombre + ",Que bonito es tu nombre")
+```
--- codigo/bloque 1 y 2/act 1,2.py
+++
@@ -1,6 +0,0 @@
-#Programa2. programar que solicita el nombre y lo imprime con un mensaje personalizado
-#Fecha de elaboraciÓn: 20210/10/14
-#Elaborado por: ANDREA GUADALUPE JIMENEZ CARDENAS
-
-nombre = input ("Ingresa tu nombre:")
-print ("Hola" + nombre + ",Que bonito es tu nombre") 
