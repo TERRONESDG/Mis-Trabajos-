9b12f1bfb317c0205f8d15b3ab2739bf6afbf378
Diego Andres Terrones Gonzalez <diegoterrones221@gmail.com>
2024-12-02 16:57:18 -0600

    Update and rename act 1,10.py to Programa10.md

---
+++ codigo/bloque 1 y 2/Programa10.md
@@ -0,0 +1,7 @@
+#0 UNICODE
+
+
+
+```python
+print ("Estoy feliz \t\u263A\t\t\u265D")
+print ("Alineado \t\u263A\t\t\u265D")
--- codigo/bloque 1 y 2/act 1,10.py
+++
@@ -1,8 +0,0 @@
-#Programa 10 UNICODE
-#Fecha de elaboraciÓn: 20210/10/16
-#Elaborado por: ANDREA GUADALUPE JIMENEZ CARDENAS
-
-
-
-print ("Estoy feliz \t\u263A\t\t\u265D")
-print ("Alineado \t\u263A\t\t\u265D")
