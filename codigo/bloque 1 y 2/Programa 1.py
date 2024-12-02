b3d9f16c6150236c7c57ce2fd4053c6d1865dae1
Diego Andres Terrones Gonzalez <diegoterrones221@gmail.com>
2024-12-02 16:40:29 -0600

    Update and rename act 1,1.py to Programa 1.py

---
+++ codigo/bloque 1 y 2/Programa 1.py
@@ -0,0 +1,5 @@
+#programa1: Imprime tu nombre en pantalla
+#Fecha de elaboraciÓn: 20210/10/14
+#Elaborado por: Diego Andres Terrones Gonzalez
+
+print ("Hola, mi nombre es DIEGO TERRONES")
--- codigo/bloque 1 y 2/act 1,1.py
+++
@@ -1,5 +0,0 @@
-#programa1: Imprime tu nombre en pantalla
-#Fecha de elaboraciÓn: 20210/10/14
-#Elaborado por: ANDREA GUADALUPE JIMENEZ CARDENAS
-
-print ("Hola, mi nombre es ANDREA JIMENEZ")
