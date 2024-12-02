d68eb109b4b20fa3334e8287db38a568766d7898
Diego Andres Terrones Gonzalez <diegoterrones221@gmail.com>
2024-12-02 17:13:54 -0600

    Update and rename ejercicio 2,3.py to programa2.3.md

--- codigo/bloque 1 y 2/ejercicio 2,3.py
+++
@@ -1,6 +0,0 @@
-#Programa.3 Comprueba el funcionamiento de la adiccion y concatenacón de varias variables
-#Fecha de elaboraciÓn: 20210/10/22
-#Elaborado por: ANDREA GUADALUPE JIMENEZ CARDENAS
-
-
-print("Mi" + "nombre"+ "es" + "Edinguer")
---
+++ codigo/bloque 1 y 2/programa2.3.md
@@ -0,0 +1,4 @@
+# Programa.3 Comprueba el funcionamiento de la adiccion y concatenacón de varias variables
+
+```python
+print("Mi" + "nombre"+ "es" + "Edinguer")
