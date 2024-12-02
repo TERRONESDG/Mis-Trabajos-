49be502b518b01a261c51f967a76fdca339726b5
Diego Andres Terrones Gonzalez <diegoterrones221@gmail.com>
2024-12-02 16:55:19 -0600

    Update and rename act 1,9 .py to Programa.9.md

---
+++ codigo/bloque 1 y 2/Programa.9.md
@@ -0,0 +1,8 @@
+# Programa.9  variables
+
+
+```python
+num = 38
+num = num + 1
+
+print (str (num))
--- codigo/bloque 1 y 2/act 1,9 .py
+++
@@ -1,8 +0,0 @@
-#Programa.9  variables
-#Fecha de elaboraciÓn: 20210/10/16
-#Elaborado por: ANDREA GUADALUPE JIMENEZ CARDENAS
-
-num = 38
-num = num + 1
-
-print (str (num))
