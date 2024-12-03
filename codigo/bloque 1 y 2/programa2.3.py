3ad0c034ee80ce9f8c0cd3336a324a3185ff887e
Diego Andres Terrones Gonzalez <diegoterrones221@gmail.com>
2024-12-02 20:37:39 -0600

    Update programa2.3.py

--- codigo/bloque 1 y 2/programa2.3.py
+++ codigo/bloque 1 y 2/programa2.3.py
@@ -1,4 +1,4 @@
- Programa.3 Comprueba el funcionamiento de la adiccion y concatenacón de varias variables
+ Programa 3 Comprueba el funcionamiento de la adiccion y concatenacón de varias variables
 
 
 print("Mi" + "nombre"+ "es" + "Edinguer")
