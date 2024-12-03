1169038cda918bd7a0392ebcba2d1c527e595cfe
Diego Andres Terrones Gonzalez <diegoterrones221@gmail.com>
2024-12-02 20:15:39 -0600

    Update and rename programa2.1.md to programa2.1.py

--- codigo/bloque 1 y 2/programa2.1.py
+++ codigo/bloque 1 y 2/programa2.1.py
@@ -1,10 +1,10 @@
-# Programa.1 Compuerta  NOT
+Programa.1 Compuerta  NOT
 
-```python
+
 print("Imprime el contrario a TRUE")
 print(not True ) #imprime falso
 print("\n")
 
 print("Imprime la negacion de fALSE")
 print(not False) # imorime verdadero
-```
+
