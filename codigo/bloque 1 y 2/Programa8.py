94f1ba7e2a20bd3f7fc1454f47ff9689beffa524
Diego Andres Terrones Gonzalez <diegoterrones221@gmail.com>
2024-12-02 22:58:10 -0600

    Update and rename Programa8.md to Programa8.py

--- codigo/bloque 1 y 2/Programa8.py
+++ codigo/bloque 1 y 2/Programa8.py
@@ -1,6 +1,6 @@
-# Programa.8 calcule las horas, minutos y mese que han pasado al ingresar una cantidad de dias
+Programa 8 calcule las horas, minutos y mese que han pasado al ingresar una cantidad de dias
 
-```python
+
 dias = float (input("Ingresa el numero de dias:"))
 horas = dias * 24
 minutos = horas * 60
@@ -9,4 +9,4 @@ meses = dias / 30
 print ("El resultado de horas es:" , str (horas))
 print ("El resultado de minutos es:" , str (minutos))
 print ("El resultado de meses es:" , str (meses))
-```
+
